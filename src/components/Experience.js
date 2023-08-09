cae7726a837aac4f8f5c7351b6064914683b12de
rbabaci1 <4rabah@gmail.com>
2023-08-09 16:36:57 -0700

    .

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -56,56 +56,24 @@ const Experience = () => {
         />
         <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
           <Details
-            position='Software Engineer'
-            company='Google'
-            companyLink='https://google.com'
+            position='Full Stack Web Developer'
+            company='Smoove'
+            companyLink='https://smoove.homes'
             time='2022-Present'
-            address='Mountain View, CA'
+            address='Oakland, CA'
             work="Worked on a team responsible for developing new features for Google's search engine, including improving the accuracy and relevance of search results and developing new tools for data analysis and visualization."
           />
 
           <Details
-            position='Intern'
-            company='Facebook'
-            companyLink='https://facebook.com'
-            time='Summer 2021'
-            address='Menlo Park, CA.'
+            position='Freelance Software Developer'
+            company='UpWork'
+            companyLink='https://upwork.com'
+            time='December 2021-November 2022'
+            address='Remote'
             work='Worked on a team responsible for developing a new mobile app
               feature that allowed users to create and share short-form video
               content, including designing and implementing a new user interface
               and developing the backend infrastructure to support the feature.'
-          />
-
-          <Details
-            position='Software Developer'
-            company='Amazon'
-            companyLink='https://amazon.com'
-            time='2020-2021'
-            address='Seattle, WA.'
-            work="Worked on a team responsible for developing Amazon's mobile app, including implementing new features such as product recommendations and user reviews, and optimizing the app's performance and reliability."
-          />
-
-          <Details
-            position='Software Developer Intern'
-            company='Microsoft'
-            companyLink='https://microsoft.com'
-            time='Summer 2019'
-            address='Redmond, WA.'
-            work="Worked on a team responsible for developing new features for
-              Microsoft's Windows operating system, including implementing a new
-              user interface for a system settings panel and optimizing the
-              performance of a core system component."
-          />
-
-          <Details
-            position='Teaching Assistant'
-            company='MIT'
-            companyLink='https://mit.edu'
-            time='Fall 2018'
-            address='Massachusetts Ave, Cambridge, MA.'
-            work='Assisted in teaching a course on computer programming, held office
-              hours to help students with assignments, and graded exams and
-              assignments.'
           />
         </ul>
       </div>
