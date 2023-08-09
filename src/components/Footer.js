155d8f99562260e4b40a8cdd714508434a70ec99
rbabaci1 <4rabah@gmail.com>
2023-08-09 12:17:17 -0700

    update and remove unused items in Navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,20 @@
-import Link from "next/link";
-import React from "react";
-import Layout from "./Layout";
+import Link from 'next/link';
+import Layout from './Layout';
 
 const Footer = () => {
   return (
     <footer
-      className="w-full border-t-2 border-solid border-dark
+      className='w-full border-t-2 border-solid border-dark
     font-medium text-lg dark:text-light dark:border-light sm:text-base
-    "
+    '
     >
-      <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
+      <Layout className='py-8 flex items-center justify-evenly lg:flex-col lg:py-6'>
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
 
-        <div className="flex items-center lg:py-2">
-          Built with <span className="text-primary text-2xl px-1 dark:text-primaryDark">&#9825;	</span> by&nbsp;
-          <Link
-            href="https://devdreaming.com"
-            target="_blank"
-            className="underline underline-offset-2"
-          >
-            CodeBucks
-          </Link>
-        </div>
-
         <Link
-          href="https://devdreaming.com/about"
-          target="_blank"
-          className="underline underline-offset-2"
+          href='https://devdreaming.com/about'
+          target='_blank'
+          className='underline underline-offset-2'
         >
           Say Hello
         </Link>
