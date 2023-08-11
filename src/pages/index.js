9e2fe947d798561730e99b1aa0a9fa0390393d3b
rbabaci1 <4rabah@gmail.com>
2023-08-11 14:09:59 -0700

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,10 +44,10 @@ export default function Home() {
                 className='!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl'
               />
               <p className='my-4 text-base font-medium md:text-sm sm:!text-xs'>
-                As a proficient full-stack developer, I'm committed to realizing
-                ideas as cutting-edge software applications. Explore my latest
-                projects and articles, highlighting expertise in software
-                development and technologies such as React.js!
+                As a proficient full-stack developer, I&apos;m committed to
+                realizing ideas as cutting-edge software applications. Explore
+                my latest projects and articles, highlighting expertise in
+                software development and technologies such as React.js!
               </p>
 
               <div className='mt-2 flex items-center self-start lg:self-center'>
