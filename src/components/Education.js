6ff0fff57f66b1b8f31d45e881920845f825da29
rabah babaci <4rabah@gmail.com>
2024-02-14 15:05:46 -0800

    update work experience and education sections

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,69 +1,67 @@
-import React, { useRef } from 'react';
-import { motion, useScroll } from 'framer-motion';
-import AboutIcon from './LiIcon';
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
+import AboutIcon from "./LiIcon";
 
 const Details = ({ type, time, place, info }) => {
-  const ref = useRef(null);
-  return (
-    <li
-      ref={ref}
-      className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]'
-    >
-      <AboutIcon reference={ref} />
-      <motion.div
-        initial={{ y: 50 }}
-        whileInView={{ y: 0 }}
-        transition={{ duration: 0.5, type: 'spring' }}
-      >
-        <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>
-          {type}
-        </h3>
-        <span className='capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm'>
-          {time} | {place}
-        </span>
-        <p className='font-medium w-full md:text-sm'>{info}</p>
-      </motion.div>
-    </li>
-  );
+	const ref = useRef(null);
+	return (
+		<li
+			ref={ref}
+			className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]">
+			<AboutIcon reference={ref} />
+			<motion.div
+				initial={{ y: 50 }}
+				whileInView={{ y: 0 }}
+				transition={{ duration: 0.5, type: "spring" }}>
+				<h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+					{type}
+				</h3>
+				<span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
+					{time} | {place}
+				</span>
+				<p className="font-medium w-full md:text-sm">{info}</p>
+			</motion.div>
+		</li>
+	);
 };
 
 const Education = () => {
-  const ref = useRef(null);
+	const ref = useRef(null);
 
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ['start end', 'center start'],
-  });
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start end", "center start"],
+	});
 
-  return (
-    <div className='my-40'>
-      <h2 className='font-bold text-8xl mb-20 w-full text-center md:text-6xl xs:text-4xl md:mb-16'>
-        Education
-      </h2>
+	return (
+		<div className="my-40">
+			<h2 className="font-bold text-8xl mb-20 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+				Education
+			</h2>
 
-      <div ref={ref} className='relative w-[75%] mx-auto lg:w-[90%] md:w-full'>
-        <motion.div
-          className='absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-dark  origin-top rounded-full dark:bg-primaryDark dark:shadow-3xl'
-          style={{ scaleY: scrollYProgress }}
-        />
-        <ul className='w-full flex flex-col items-start justify-between ml-4'>
-          <Details
-            type='Full-Stack Web Development BootCamp'
-            time='2020-2021'
-            place='Bloom Institute of Technology'
-            info='Completed a 9+ month Computer Science & Software Engineering Program that provides an immersive hands-on curriculum with a focus on computer science, software engineering, and web development.'
-          />
+			<div ref={ref} className="relative w-[75%] mx-auto lg:w-[90%] md:w-full">
+				<motion.div
+					className="absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-dark  origin-top rounded-full dark:bg-primaryDark dark:shadow-3xl"
+					style={{ scaleY: scrollYProgress }}
+				/>
+				<ul className="w-full flex flex-col items-start justify-between ml-4">
+					<Details
+						type="Full-Stack Web Development Bootcamp"
+						time="2019-2021"
+						place="Bloom Institute of Technology"
+						info="Completed a Full-Stack Web Development bootcamp at Bloomtech Institute, focusing on React, Redux, and Agile methodologies. Acquired hands-on skills for creating scalable web apps and adapting to the tech industry's pace."
+					/>
 
-          <Details
-            type='Software Engineering Training'
-            time='2018-2019'
-            place='42 Silicon Valley'
-            info='Acquired expertise in software engineering, web development, and computer science in a peer-to-peer learning environment. Thrived in the intensive Piscine month, mastering crucial software engineering basics in a collaborative environment.'
-          />
-        </ul>
-      </div>
-    </div>
-  );
+					<Details
+						type="Bachelors of Civil Engineering Technology Technician"
+						time="2012-2016"
+						place="Université Mouloud Mammeri Tizi-Ouzou Algeria"
+						info="Completed Civil Engineering Technology program, focusing on structural design, construction management, and CAD. Gained key analytical and problem-solving skills, bridging engineering principles with software development. Prepared for tech industry challenges through practical learning."
+					/>
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Education;
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,82 +1,79 @@
-import React, { useRef } from 'react';
-import { motion, useScroll } from 'framer-motion';
-import LiIcon from './LiIcon';
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
+import LiIcon from "./LiIcon";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
-  const ref = useRef(null);
+	const ref = useRef(null);
 
-  return (
-    <li
-      ref={ref}
-      className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]'
-    >
-      <LiIcon reference={ref} />
+	return (
+		<li
+			ref={ref}
+			className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]">
+			<LiIcon reference={ref} />
 
-      <motion.div
-        initial={{ y: 50 }}
-        whileInView={{ y: 0 }}
-        transition={{ duration: 0.5, type: 'spring' }}
-      >
-        <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>
-          {position}{' '}
-          <a
-            className='capitalize text-primary dark:text-primaryDark'
-            href={companyLink}
-            target={'_blank'}
-          >
-            @{company}
-          </a>
-        </h3>
-        <span className='capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm'>
-          {time} | {address}
-        </span>
-        <p className='font-medium w-full md:text-sm'> {work}</p>
-      </motion.div>
-    </li>
-  );
+			<motion.div
+				initial={{ y: 50 }}
+				whileInView={{ y: 0 }}
+				transition={{ duration: 0.5, type: "spring" }}>
+				<h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+					{position}{" "}
+					<a
+						className="capitalize text-primary dark:text-primaryDark"
+						href={companyLink}
+						target={"_blank"}>
+						@{company}
+					</a>
+				</h3>
+				<span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
+					{time} | {address}
+				</span>
+				<p className="font-medium w-full md:text-sm"> {work}</p>
+			</motion.div>
+		</li>
+	);
 };
 
 const Experience = () => {
-  const ref = useRef(null);
+	const ref = useRef(null);
 
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ['start end', 'center start'],
-  });
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start end", "center start"],
+	});
 
-  return (
-    <div className='my-0'>
-      <h2 className='font-bold text-8xl mb-20 w-full text-center md:text-6xl xs:text-4xl md:mb-16'>
-        Experience
-      </h2>
+	return (
+		<div className="my-0">
+			<h2 className="font-bold text-8xl mb-20 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+				Experience
+			</h2>
 
-      <div ref={ref} className='relative w-[75%] mx-auto lg:w-[90%] md:w-full'>
-        <motion.div
-          className='absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-dark origin-top  dark:bg-primaryDark dark:shadow-3xl'
-          style={{ scaleY: scrollYProgress }}
-        />
-        <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
-          <Details
-            position='Full Stack Web Developer'
-            company='Smoove'
-            companyLink='https://smoove.homes'
-            time='November 2022-November 2023'
-            address='Hybrid'
-            work="Integral in creating Smoove's online moving platform. Led front-end and back-end work, enabling user-friendly features like registration, booking, tracking, and payments. Collaborated for seamless UX, optimized visuals, and effective testing. Aligned with stakeholders for brand cohesion and impactful outcome."
-          />
+			<div ref={ref} className="relative w-[75%] mx-auto lg:w-[90%] md:w-full">
+				<motion.div
+					className="absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-dark origin-top  dark:bg-primaryDark dark:shadow-3xl"
+					style={{ scaleY: scrollYProgress }}
+				/>
+				<ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
+					<Details
+						position="Founder and Lead Product Developer"
+						company="Smoove"
+						companyLink="https://smoove.homes"
+						time="November 2022 - December 2023"
+						address="Hybrid"
+						work="Founded Smoove to transform the moving experience with a pioneering online platform, Smoove.homes. Spearheaded full-stack development, integrating Next.js, Twilio, Mapbox, and Firebase to deliver a seamless service from booking to payment. Collaborated on UX/UI for an intuitive user journey, and innovated operational systems using Slack and Stripe, enhancing efficiency and security. Drove continuous engagement, building a loyal community through targeted communications."
+					/>
 
-          <Details
-            position='Software Developer'
-            company='UpWork'
-            companyLink='https://upwork.com'
-            time='June 2021-November 2022'
-            address='Remote'
-            work='As an Upwork freelance web developer, I design and build company websites. I create wirep-frames, optimize graphics, and collaborate with clients to align with their brand and goals. Thorough testing and debugging ensure seamless website functionality, polished, functional sites.'
-          />
-        </ul>
-      </div>
-    </div>
-  );
+					<Details
+						position="Full Stack Web Developer"
+						company="Yassir"
+						companyLink="https://yassir.com"
+						time="March 2021 - November 2022"
+						address="Remote"
+						work="At Yassir, I played a pivotal role as a Full Stack Web Developer, enhancing our super app with user-driven features like an efficient ticket system for drivers, boosting service quality. Leveraged JavaScript and React.js for fluid front-end interfaces, while also fortifying back-end and database integrity. My efforts were key in fostering a culture of innovation, responding dynamically to the evolving needs of our diverse user base."
+					/>
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Experience;
